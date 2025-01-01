0786177947b5785dab1c62b872ed86527aca6402
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2025-01-01 11:47:05 +0500

    Minor Changes

--- lib/single_message.dart
+++ lib/single_message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:voice_assistant_with_ai/colors.dart';
 
 class SingleMessage extends StatelessWidget {
@@ -26,27 +27,29 @@ class SingleMessage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Container(
-              alignment: Alignment.centerRight,
-              constraints: BoxConstraints(
-                maxWidth: width * 0.65,
+            alignment: Alignment.centerRight,
+            constraints: BoxConstraints(
+              maxWidth: width * 0.65,
+            ),
+            padding: EdgeInsets.symmetric(
+                horizontal: height * 0.0125, vertical: height * 0.0125),
+            decoration: BoxDecoration(
+              color: Pallete.firstSuggestionBoxColor,
+              border: Border.all(
+                color: Colors.black.withOpacity(0.5),
               ),
-              padding: EdgeInsets.symmetric(
-                  horizontal: height * 0.0125, vertical: height * 0.0125),
-              decoration: BoxDecoration(
-                color: Pallete.firstSuggestionBoxColor,
-                border: Border.all(
-                  color: Colors.black.withOpacity(0.5),
-                ),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                  bottomLeft: Radius.circular(15),
-                ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+                bottomLeft: Radius.circular(15),
               ),
-              child: Text(
-                message.toString(),
-                style: TextStyle(fontSize: 16),
-              ))
+            ),
+            // child: Text(
+            //   message.toString(),
+            //   style: TextStyle(fontSize: 16),
+            // ),
+            child: MarkdownBody(data: message),
+          )
         ],
       ),
     );
@@ -64,27 +67,72 @@ class SingleMessage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-              alignment: Alignment.centerLeft,
-              constraints: BoxConstraints(maxWidth: width * 0.65),
-              padding: EdgeInsets.symmetric(
-                  horizontal: height * 0.0125, vertical: height * 0.0125),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black.withOpacity(0.5),
-                ),
-                color: Pallete.secondSuggestionBoxColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                  bottomRight: Radius.circular(15),
-                ),
+            alignment: Alignment.centerLeft,
+            constraints: BoxConstraints(maxWidth: width * 0.65),
+            padding: EdgeInsets.symmetric(
+                horizontal: height * 0.0125, vertical: height * 0.0125),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black.withOpacity(0.5),
               ),
-              child: Text(
-                message.toString(),
-                style: TextStyle(fontSize: 16),
-              )),
+              color: Pallete.secondSuggestionBoxColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+            ),
+            // child: Text(
+            //   message.toString(),
+            //   style: TextStyle(fontSize: 16),
+            // ),
+            // child: BoldTextParser(message),
+            child: MarkdownBody(data: message),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class BoldTextParser extends StatelessWidget {
+  final String text;
+
+  BoldTextParser(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    List<TextSpan> spans = [];
+    RegExp exp = RegExp(r'\*\*(.*?)\*\*');
+    Iterable<Match> matches = exp.allMatches(text);
+    int lastMatchEnd = 0;
+
+    for (Match match in matches) {
+      // Add the normal text before the bold text
+      if (match.start > lastMatchEnd) {
+        spans.add(TextSpan(
+          text: text.substring(lastMatchEnd, match.start),
+          style: TextStyle(color: Colors.black),
+        ));
+      }
+      // Add the bold text
+      spans.add(TextSpan(
+        text: match.group(1),
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ));
+      lastMatchEnd = match.end;
+    }
+
+    // Add the remaining normal text
+    if (lastMatchEnd < text.length) {
+      spans.add(TextSpan(
+        text: text.substring(lastMatchEnd),
+        style: TextStyle(color: Colors.black),
+      ));
+    }
+
+    return RichText(
+      text: TextSpan(children: spans),
     );
   }
 }
